47b01183d911738de64cec34d1167efa76a499b2
Shubham <shubhamgupta43567@gmail.com>
2026-04-09 16:44:12 +0530

    Insurance Predictor API

---
+++ backend.py
@@ -0,0 +1,44 @@
+from schema.user_input import UserInput
+from model.predict import predict_output,model,MODEL_VERSION
+from schema.prediction_response import PredictionResponse
+import pickle
+import pandas as pd 
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+
+  
+app = FastAPI()
+
+@app.get('/')
+def home():
+  return {"message":"Insurance Prediction API"}
+
+@app.get('/health')
+def health_check():
+  return {
+    "status":'OK',
+    "version":MODEL_VERSION,
+    "model_loaded":model is not None
+      }
+    
+@app.post('/predict', response_model=PredictionResponse)
+def predict(data: UserInput):
+  user_input = {
+    "bmi":data.bmi,
+    "age_group":data.age_group,
+    "lifestyle_risk":data.lifestyle_risk,
+    "city_tier":data.city_tier,
+    "income_lpa":data.income_lpa,
+    "occupation":data.occupation
+  }
+  
+  try:
+    
+    prediction = predict_output(user_input=user_input)
+    return JSONResponse(status_code=200, content={"Predicted Category": prediction})
+  
+  except Exception as e:
+    return JSONResponse(status_code=400, content={"Error Message":str(e)})
+  
+  
